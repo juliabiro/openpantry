da5f7565db032aade79f6234aa9820ba3423a4d8
Julia Biro <julia.biro@gmail.com>
2014-04-06 16:47:11 +0200

    showing list items in lists details page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Openpantry::Application.routes.draw do
 
   resources :list_items
 root :to => "lists#index"
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
